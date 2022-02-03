59f6ec2cd90c50fdd1b5d100a2d68ea85a4ae7fa
AlexFIare <42217154+AlexFIare@users.noreply.github.com>
2022-02-03 09:20:12 -0500

    alexis
    el código que coje el dato en bruto y transforama en datos de lat y lng

---
+++ gps.py
@@ -0,0 +1,21 @@
+import serial
+import time
+import string
+import pynmea2
+import sys
+
+while True:
+	port="/dev/ttyAMA0"
+	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	dataout = pynmea2.NMEAStreamReader()
+	newdata=ser.readline()
+
+  if sys.version_info == 3:
+    newdata = newdata.decode("utf-8","ignore")
+    
+	if newdata[0:6] == "$GPRMC":
+		newmsg=pynmea2.parse(newdata)
+		lat=newmsg.latitude
+		lng=newmsg.longitude
+		gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+		print(gps)
